5c60af136feec5a18527ecec8405e83a16661eea
kimminkyu <elliotelon@gmail.com>
2022-03-13 21:10:05 +0900

    헤더 로고 만들기

---
+++ components/Header.tsx
@@ -0,0 +1,72 @@
+import React from "react";
+import styled from "styled-components";
+import Link from "next/link";
+import AirbnbLogoIcon from "../public/static/svg/logo/logo.svg";
+import AirbnbLogoTextIcon from "../public/static/svg/logo/logo_text.svg";
+import palette from "../styles/palette";
+
+const Container = styled.div`
+  position: sticky;
+  top: 0;
+  width: 100%;
+  height: 80px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 80px;
+  background-color: white;
+  box-shadow: rgba(0, 0, 0, 0.08) 0px 1px 12px;
+  z-index: 10;
+  .header-logo-wrapper {
+    display: flex;
+    align-items: center;
+    .header-logo {
+      margin-right: 6px;
+    }
+  }
+  .header-auth-buttons {
+    .header-sign-up-button {
+      height: 42px;
+      margin-right: 8px;
+      padding: 0 16px;
+      border: 0;
+      border-radius: 21px;
+      background-color: white;
+      cursor: pointer;
+      outline: none;
+      font-weight: 600;
+      &:hover {
+        background-color: ${palette.gray_f7};
+      }
+    }
+    .header-login-button {
+      height: 42px;
+      padding: 0 16px;
+      border: 0;
+      box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.18);
+      border-radius: 21px;
+      background-color: white;
+      cursor: pointer;
+      outline: none;
+      font-weight: 600;
+      &:hover {
+        box-shadow: 0px 2px 8px rgba(0, 0, 0, 0.12);
+      }
+    }
+  }
+`;
+
+const Header: React.FC = () => {
+  return (
+    <Container>
+      <Link href="/">
+        <a className="haeder-logo-wrapper">
+          <AirbnbLogoIcon className="header-logo" />
+          <AirbnbLogoTextIcon />
+        </a>
+      </Link>
+    </Container>
+  );
+};
+
+export default Header;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,7 @@
 import React from "react";
-import styled from "styled-components";
-
-const Container = styled.div`
-  font-size: 21px;
-  color: gray;
-`;
 
 const index: React.FC = () => {
-  return <Container>hello, world</Container>;
+  return <div> </div>;
 };
 
 export default index;
