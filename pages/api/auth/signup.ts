9a7d758a8cf2bae9752d0190b6fcd5d7fd589404
kimminkyu <elliotelon@gmail.com>
2022-03-14 19:27:16 +0900

    JWT 토큰 생성

--- pages/api/auth/signup.ts
+++ pages/api/auth/signup.ts
@@ -1,5 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 import Data from "../../../lib/data";
 import { StoredUserType } from "../../../types/user";
 
@@ -37,6 +38,8 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     };
 
     Data.user.write([...users, newUser]);
+
+    const token = jwt.sign(String(newUser.id), process.env.JWT_SECRET!);
 
     return res.end();
   }
