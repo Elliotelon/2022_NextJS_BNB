07794f78f5290d67a4f4f206f396c8fea54085cb
kimminkyu <elliotelon@gmail.com>
2022-03-14 17:30:06 +0900

    회원가입 셀렉터

--- components/auth/SignUpModal.tsx
+++ components/auth/SignUpModal.tsx
@@ -6,6 +6,9 @@ import PersonIcon from "../../public/static/svg/auth/person.svg";
 import OpenedEyeIcon from "../../public/static/svg/auth/opened_eye.svg";
 import ClosedEyeIcon from "../../public/static/svg/auth/closed_eye.svg";
 import Input from "../common/Input";
+import { dayList, monthList, yearList } from "../../lib/staticData";
+import palette from "../../styles/palette";
+import Selector from "../common/Selector";
 
 const Container = styled.form`
   width: 568px;
@@ -29,6 +32,31 @@ const Container = styled.form`
       cursor: pointer;
     }
   }
+  .sign-up-birthday-label {
+    font-size: 16px;
+    font-weight: 600;
+    margin-top: 16px;
+    margin-bottom: 8px;
+  }
+  .sign-up-modal-birthday-info {
+    margin-bottom: 16px;
+    color: ${palette.charcoal};
+  }
+  .sign-up-modal-birthday-selectors {
+    display: flex;
+    margin-bottom: 24px;
+    .sign-up-modal-birthday-year-selector {
+      margin-right: 16px;
+      flex-grow: 1;
+    }
+    .sign-up-modal-birthday-month-selector {
+      margin-right: 16px;
+      width: 25%;
+    }
+    .sign-up-modal-birthday-day-selector {
+      width: 33.3333%;
+    }
+  }
 `;
 
 const SignUpModal: React.FC = () => {
@@ -37,6 +65,9 @@ const SignUpModal: React.FC = () => {
   const [firstname, setFirstname] = useState("");
   const [password, setPassword] = useState("");
   const [hidePassword, setHidePassword] = useState(true);
+  const [birthYear, setBirthYear] = useState<string | undefined>();
+  const [birthMonth, setBirthMonth] = useState<string | undefined>();
+  const [birthDay, setBirthDay] = useState<string | undefined>();
 
   //* 이메일 주소 변경시
   const onChangeEmail = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -57,9 +88,25 @@ const SignUpModal: React.FC = () => {
   const onChangePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
     setPassword(event.target.value);
   };
+
   //*비밀번호 숨김 토글
   const toggleHidePassword = () => {
     setHidePassword(!hidePassword);
+  };
+
+  //* 생년월일 년 변경시
+  const onChangeBirthYear = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setBirthYear(event.target.value);
+  };
+
+  //* 생년월일 월 변경시
+  const onChangeBirthMonth = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setBirthMonth(event.target.value);
+  };
+
+  //* 생년월일 일 변경시
+  const onChangeBirthDay = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setBirthDay(event.target.value);
   };
 
   return (
@@ -105,6 +152,40 @@ const SignUpModal: React.FC = () => {
           value={password}
           onChange={onChangePassword}
         />
+      </div>
+      <p className="sign-up-birthdat-label">생일</p>
+      <p className="sign-up-modal-birthday-info">
+        만 18세 이상의 성인만 회원으로 가입할 수 있습니다. 생일은 다른
+        에어비앤비 이용자에게 공개되지 않습니다.
+      </p>
+      <div className="sign-up-modal-birthday-selectors">
+        <div className="sign-up-modal-birthday-year-selector">
+          <Selector
+            options={yearList}
+            disabledOptions={["년"]}
+            defaultValue="년"
+            value={birthYear}
+            onChange={onChangeBirthYear}
+          />
+        </div>
+        <div className="sign-up-modal-birthday-month-selector">
+          <Selector
+            options={monthList}
+            disabledOptions={["월"]}
+            defaultValue="월"
+            value={birthMonth}
+            onChange={onChangeBirthMonth}
+          />
+        </div>
+        <div className="sign-up-modal-birthday-day-selector">
+          <Selector
+            options={dayList}
+            disabledOptions={["일"]}
+            defaultValue="일"
+            value={birthDay}
+            onChange={onChangeBirthDay}
+          />
+        </div>
       </div>
     </Container>
   );
